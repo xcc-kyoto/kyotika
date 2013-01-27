966db3d87c8ad400523ac96e2bd0eef1a6368ef5
Takahiro Kunii <pub.xcc@gmail.com>
2013-01-27 10:31:28 +0900

    プロジェクト追加

---
+++ kyotika/KMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KMAppDelegate.h
+//  kyotika
+//
+//  Created by kunii on 2013/01/27.
+//  Copyright (c) 2013年 國居貴浩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
